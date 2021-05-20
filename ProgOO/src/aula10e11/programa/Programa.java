25e822b9d2418d1d09c97432c8554fd9f30bad8f
allan <allan@192.168.51.173>
2021-05-20 13:08:03 -0300

    Adicionando CSS

--- ProgOO/src/aula10e11/programa/Programa.java
+++ ProgOO/src/aula10e11/programa/Programa.java
@@ -1,9 +1,21 @@
 package aula10e11.programa;
 
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+
 public class Programa {
 
 	public static void main(String[] args) {
-
+		
+		String[] diasSemanaAbreviado = DateFormatSymbols.getInstance().getShortWeekdays();
+		
+		for (int i=1; i <= diasSemanaAbreviado.length-1; i++) {
+			System.out.println(diasSemanaAbreviado[i]);
+		}
+		
+		System.out.println(Arrays.toString(diasSemanaAbreviado));
+		System.out.println(diasSemanaAbreviado[0]);
+				
 	}
 
 }
---
+++ ProgOO/src/aula13/Teste.java
@@ -0,0 +1,15 @@
+package aula13;
+
+import java.util.List;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+
+	}
+
+}
